ff7f24f7c1e1b480b1b57824297ce9873b1e9afc
nikitasirotkin <ownikss@yandex.ru>
2022-07-28 13:53:38 +0300

    chore: update tsconfig

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ import FirebaseSettings from './domain/firebaseSettings'
 import OnboardingController from './controllers/onboardingController'
 import EveryoneController from './controllers/everyoneController'
 
-const config = !process.env.PRODUCTION && require('./config.json')
+const config = require('./config.json')
 const settings = new SettingsRepository(config)
 
 const mentionBuilder = new MentionBuilder(settings)
